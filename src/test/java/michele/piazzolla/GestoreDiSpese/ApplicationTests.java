6f95da2c3142527912c27912786e5ce48a854d70
Michele Piazzolla <vecio88@gmail.com>
2021-02-02 13:39:22 +0100

    Modificate relazioni

--- src/test/java/michele/piazzolla/GestoreDiSpese/ApplicationTests.java
+++ src/test/java/michele/piazzolla/GestoreDiSpese/ApplicationTests.java
@@ -1,13 +1,33 @@
 package michele.piazzolla.GestoreDiSpese;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.FixMethodOrder;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import michele.piazzolla.GestoreDiSpese.Entities.Movement;
+import michele.piazzolla.GestoreDiSpese.Repositories.MovementRepository;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ApplicationTests {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class MovementTest {
+	
+	@Autowired
+	private MovementRepository movementRepo;
 
 	@Test
-	void contextLoads() {
+	public void A_TestSelByIdMovement() {
+		
+		Movement movement = movementRepo.findByIdMovement(1);
+		assertNotNull(movement);
+		
 	}
 
 }
